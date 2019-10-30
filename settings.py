059c111574b014f2ef0dc511956e5a7941cb93ce
albertbrook <3226521643@qq.com>
2019-10-30 23:24:31 +0800

    add settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings(object):
+    def __init__(self):
+        self.screen_size = (1280, 720)
+
+        self.line_size = 3
+        self.block_size = 60
+
+        self.compass_line_color = (255, 255, 255)
+        self.select_line_color = (255, 0, 0)
